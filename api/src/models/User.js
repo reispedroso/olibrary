e06cb2b1ae1b776c698b45bad0725d197d4c6df4
reispedroso <eduardopedroso3d2y@gmail.com>
2024-04-24 16:27:44 -0300

    user update trial

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -3,10 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  createdAt: { type: Date, default: null },
-  updatedAt: { type: Date, default: null },
-  deletedAt: { type: Date, default: null },
+  password: { type: String, required: true }
 });
 
 module.exports = mongoose.model("User", userSchema);
